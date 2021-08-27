ba969b6e827ceafc4743f5b6a2b58f41541feedb
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-27 22:18:28 +0700

    create new UserModel to interact with database

---
+++ app/Models/UserModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+	public function getUserProfile($data)
+	{
+		return $this->db->table('user-profile')->where(['nis' => $data])->get()->getRowArray();
+	}
+
+	public function getDataPonsel($data)
+	{
+		return $this->db->table('data-ponsel')->where(['nis' => $data])->get()->getRowArray();
+	}
+
+	public function updateProfile($data)
+	{
+		$this->db->table('user-profile')->where(['nis' => $data['nis']])->update($data);
+	}
+
+	public function updatePonsel($data)
+	{
+		$this->db->table('data-ponsel')->where(['nis' => $data['nis']])->update($data);
+	}
+}

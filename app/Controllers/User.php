c9d1fde7c25c8de1ad7182cbda20003ef2ae7ab8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-28 01:04:58 +0700

    add new method for change password

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -19,20 +19,6 @@ class User extends BaseController
 		return view('user/dashboard', $data);
 	}
 
-	public function home()
-	{
-		if ($this->request->isAJAX()) {
-
-			$msg = [
-				'data'	=> view('page/dashboard')
-			];
-
-			echo json_encode($msg);
-		} else {
-			exit('Halaman tidak ditemukan!');
-		}
-	}
-
 	public function profile()
 	{
 		if (!session()->get('nis')) {
@@ -217,6 +203,74 @@ class User extends BaseController
 				];
 
 				$this->userModel->updatePonsel($data);
+
+				$msg = [
+					'sukses' => 'Data berhasil diperbaharui'
+				];
+
+
+				echo json_encode($msg);
+			};
+		};
+	}
+
+	public function gantiSandi()
+	{
+		if (!session()->get('nis')) {
+			return redirect()->to('login');
+		}
+
+		$data = [
+			'title'	=> 'Ganti Kata Sandi | Verval Ponsel SMA Negeri 1 Rawamerta'
+		];
+
+		return view('user/change-password', $data);
+	}
+
+	public function gantiPassword()
+	{
+		if ($this->request->isAJAX()) {
+			$validation = \Config\Services::validation();
+
+			if (!$this->validate([
+				'password1'  => [
+					'rules'     => 'min_length[8]',
+					'errors'    => [
+						'min_length'  => 'Kata Sandi minimal 8 karakter!'
+					]
+				],
+				'password2'  => [
+					'rules'     => 'required[password2]|matches[password1]',
+					'errors'    => [
+						'required'  => 'Konfirmasi Kata Sandi wajib diisi!',
+						'matches'	=> 'Konfirmasi kata sandi tidak sesuai!'
+					]
+				],
+			])) {
+				$msg = [
+					'error' => [
+						'password1'  => $validation->getError('password1'),
+						'password2'  => $validation->getError('password2'),
+					],
+				];
+
+				echo json_encode($msg);
+			} else {
+				$sesi = [
+					'nis' 	=> session()->get('nis'),
+					'nama'	=> session()->get('nama'),
+					'kelas'	=> session()->get('kelas')
+				];
+
+				$data = [
+					'username'		=> $sesi['nis'],
+					'password' 		=> $this->request->getVar('password1'),
+					'nama'  		=> $sesi['nama'],
+					'email'			=> 'siswa@siswa.com',
+					'kelas'  		=> $sesi['kelas'],
+				];
+
+				$this->userModel->updatePassword($data);
 
 				$msg = [
 					'sukses' => 'Data berhasil diperbaharui'

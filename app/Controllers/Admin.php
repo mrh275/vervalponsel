0533455430fd2b4c72a415608dd1b3ee80add113
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-30 13:02:25 +0700

    add method to change password akun siswa

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -33,4 +33,84 @@ class Admin extends BaseController
 
 		return view('admin/data-ponsel', $data);
 	}
+
+	public function akunSiswa()
+	{
+		$data = [
+			'title'			=> 'User Siswa | Verval Ponsel SMA Negeri 1 Rawamerta',
+		];
+
+		return view('admin/akun-siswa', $data);
+	}
+
+	public function getUserAkun()
+	{
+		if ($this->request->isAJAX()) {
+
+			$data = [
+				'akunSiswa'	=> $this->adminModel->getUserSiswa()
+			];
+
+			$msg = [
+				'sukses' => view('admin/data-ponsel-table', $data),
+			];
+
+			echo json_encode($msg);
+		}
+	}
+
+	public function gantiPasswordSiswa()
+	{
+		if ($this->request->isAJAX()) {
+			$validation = \Config\Services::validation();
+
+			if (!$this->validate([
+				'password1'  => [
+					'rules'     => 'min_length[8]',
+					'errors'    => [
+						'min_length'  => 'Kata Sandi minimal 8 karakter!'
+					]
+				],
+				'password2'  => [
+					'rules'     => 'required[password2]|matches[password1]',
+					'errors'    => [
+						'required'  => 'Konfirmasi Kata Sandi wajib diisi!',
+						'matches'	=> 'Konfirmasi kata sandi tidak sesuai!'
+					]
+				],
+			])) {
+				$msg = [
+					'error' => [
+						'password1'  => $validation->getError('password1'),
+						'password2'  => $validation->getError('password2'),
+					],
+				];
+
+				echo json_encode($msg);
+			} else {
+				$sesi = [
+					'nis' 	=> $this->request->getVar('nis'),
+					'nama'	=> $this->request->getVar('nama'),
+					'kelas'	=> $this->request->getVar('kelas')
+				];
+
+				$data = [
+					'username'		=> $sesi['nis'],
+					'password' 		=> $this->request->getVar('password1'),
+					'nama'  		=> $sesi['nama'],
+					'email'			=> 'siswa@siswa.com',
+					'kelas'  		=> $sesi['kelas'],
+				];
+
+				$this->userModel->updatePassword($data);
+
+				$msg = [
+					'sukses' => 'Data berhasil diperbaharui'
+				];
+
+
+				echo json_encode($msg);
+			};
+		};
+	}
 }

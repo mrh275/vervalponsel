3c034a10db5d7fb578d4409d216b4c0c390c8668
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-30 08:27:44 +0700

    create admin model for interact with database

---
+++ app/Models/AdminModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AdminModel extends Model
+{
+	public function cekData($data)
+	{
+		return $this->db->table('admin')->where(['username' => $data['username']])->get()->getRowArray();
+	}
+
+	public function getDataPonsel()
+	{
+		return $this->db->table('data-ponsel')->get()->getResultArray();
+	}
+}

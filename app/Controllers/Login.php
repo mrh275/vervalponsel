4500920d085358deaa4c15eeda03a77f84ac3b7f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-30 08:27:26 +0700

    add admin method for handle admin login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -80,4 +80,67 @@ class Login extends BaseController
             };
         };
     }
+
+    public function admin()
+    {
+
+        if ($this->request->isAJAX()) {
+            $validation = \Config\Services::validation();
+
+            if (!$this->validate([
+                'username'  => [
+                    'rules'     => 'required[username]',
+                    'errors'    => [
+                        'required'  => 'Username wajib diisi!'
+                    ]
+                ],
+                'password'  => [
+                    'rules'     => 'required[password]',
+                    'errors'    => [
+                        'required'  => 'Password wajib diisi!'
+                    ]
+                ]
+            ])) {
+                $msg = [
+                    'error' => [
+                        'username'  => $validation->getError('username'),
+                        'password'  => $validation->getError('password')
+                    ],
+                ];
+
+                echo json_encode($msg);
+            } else {
+                $data = [
+                    'username'  => $this->request->getVar('username'),
+                    'password'  => $this->request->getVar('password')
+                ];
+
+                $dataHasil = $this->adminModel->cekData($data);
+
+                $sesi = [
+                    'username'      => $dataHasil['username'],
+                    'nama'          => $dataHasil['nama'],
+                ];
+
+                if ($dataHasil == false) {
+                    $msg = [
+                        'nihil'    => 'Akun tidak ditemukan!'
+                    ];
+                } else {
+                    if ($data['password'] != $dataHasil['password']) {
+                        $msg = [
+                            'pwError'   => 'Kata sandi salah!'
+                        ];
+                    } else {
+                        $msg = [
+                            'sukses'    => 'Login berhasil'
+                        ];
+                        session()->set($sesi);
+                    }
+                }
+
+                echo json_encode($msg);
+            };
+        };
+    }
 }
